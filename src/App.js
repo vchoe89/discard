d473cb5e46f416ff717b051f0fe2e32f2f1acc52
github username <vchoe89@gmail.com>
2020-01-23 12:44:47 -0500

    changed layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { HashLink as Link } from 'react-router-hash-link';
 import './App.css';
 import Welcome from './components/Welcome.js'
 import About from './components/About.js'
@@ -8,13 +9,10 @@ import Navbar from './components/Navbar.js'
 
 function App() {
   return (
-    <Route>
-      <Switch>
-        <Welcome exact path='/'/>
-        <About ecact path='/about'/>
-      </Switch>
-      <Navbar />
-    </Route>
+    <React.Fragment>
+      <Welcome />
+      <About />
+    </React.Fragment>
 
   );
 }
