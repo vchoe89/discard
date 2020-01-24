044a5926f5940668dff629702390e326a73eb4ad
github username <vchoe89@gmail.com>
2020-01-24 16:28:09 -0500

    centered navbar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -5,16 +5,14 @@ class Navibar extends React.Component{
 
   render(){
     return(
+      <div id='nav-bar'>
       <Navbar fixed='top' bg="light" expand="lg" >
-        <Navbar.Collapse>
-        <Nav>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#projects">Projects</Nav.Link>
             <Nav.Link href="#Blog">Blog</Nav.Link>
             <Nav.Link href="#Contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
       </Navbar>
+      </div>
     )
   }
 }
