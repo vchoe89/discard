8830266bb0e60b10da51f202edb96e8c3c7b51a1
Victor Choe <vchoe89@gmail.com>
2020-01-29 14:16:13 -0500

    fixed icons

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ import Welcome from './components/Welcome.js'
 import About from './components/About.js'
 import Navibar from './components/Navibar.js'
 import Projects from './components/Projects.js'
+import Blogs from './components/Blogs.js'
+
 
 function App() {
   return (
     <React.Fragment>
-      <Navibar />
+      <Navibar  sticky='top'/>
       <Welcome />
       <About />
       <Projects />
+      <Blogs />
     </React.Fragment>
 
   );
--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Blogs extends React.Component{
   render(){
     return(
-
+      <h1 id='headlines'>Blogs</h1>
     )
   }
 }
