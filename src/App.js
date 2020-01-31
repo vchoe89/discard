e240fb9f94ff3d04ac1c53199912461c44216f11
Victor Choe <vchoe89@gmail.com>
2020-01-31 16:19:32 -0500

    cleaned up imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import { HashLink as Link } from 'react-router-hash-link';
 import './App.css';
-import { Nav } from 'react-bootstrap';
 import Welcome from './components/Welcome.js'
 import About from './components/About.js'
 import Navibar from './components/Navibar.js'
--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Nav, Navbar, NavItem, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap';
 
 class Navibar extends React.Component{
 
