d64ec8c36d72ec82ad9a35d6a97cd9d4319f9d69
Victor Choe <vchoe89@gmail.com>
2020-01-31 21:10:15 -0500

    sticky navbar

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -6,7 +6,7 @@ class Navibar extends React.Component{
   render(){
     return(
       <Navbar >
-        <div id='headlines'>
+        <div id='navibar'>
             <Nav.Link href="about">About</Nav.Link>
             <Nav.Link href="projects">Projects</Nav.Link>
             <Nav.Link href="blog">Blog</Nav.Link>
