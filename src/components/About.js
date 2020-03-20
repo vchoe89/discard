d31afba91ea12531ca551ab57235f11d3c11c858
Victor Choe <vchoe89@gmail.com>
2020-03-20 16:42:31 -0400

    adjusting bar section

--- src/components/About.js
+++ src/components/About.js
@@ -15,50 +15,52 @@ class About extends React.Component{
 
             <div id='about-icons1'>
               <i class="material-icons">speed</i>
-                <h3 id='secondary-header'>fast</h3>
+                <h3 id='secondary-header-fast'>fast</h3>
                   <p id='icon-desc'>Fast load times and high speed interaction.</p>
             </div>
 
             <div id='about-icons2' >
               <i class="material-icons">touch_app</i>
-                <h3 id='secondary-header'>responsive</h3>
+                <h3 id='secondary-header-resp'>responsive</h3>
                   <p id='icon-desc'>Layouts that work with any platform, big or small.</p>
             </div>
 
             <div id='about-icons3'>
               <i class="material-icons">offline_bolt</i>
-                <h3 id='secondary-header'>dynamic</h3>
+                <h3 id='secondary-header-dyn'>dynamic</h3>
                   <p id='icon-desc'>Interactive websites that come to life.</p>
             </div>
-            
+
           </div>
 
-            <div id='bar-section'>
-              <h2 id='bar-names'>Ruby/Rails</h2>
-              <ProgressBar animated now={50} />
-            <div id='bar-section'>
-              <h2 id='bar-names'>JavaScript</h2>
-              <ProgressBar animated now={50} />
-            </div>
-            <div id='bar-section'>
-              <h2 id='bar-names'>React</h2>
-              <ProgressBar animated now={50} />
-            </div>
-            <div id='bar-section'>
-              <h2 id='bar-names'>HTML</h2>
-              <ProgressBar animated now={50} />
-            </div>
-            <div id='bar-section'>
-              <h2 id='bar-names'>CSS</h2>
-              <ProgressBar animated now={50} />
-            </div>
-            <div id='bar-section'>
-              <h2 id='bar-names'>Redux</h2>
-              <ProgressBar animated now={50} />
+          <div id='bar-section'>
+              <div>
+                <h2 id='bar-names'>Ruby/Rails</h2>
+                <ProgressBar animated now={50} />
+              <div>
+                <h2 id='bar-names'>JavaScript</h2>
+                <ProgressBar animated now={50} />
+              </div>
+              <div>
+                <h2 id='bar-names'>React</h2>
+                <ProgressBar animated now={50} />
+              </div>
+              <div>
+                <h2 id='bar-names'>HTML</h2>
+                <ProgressBar animated now={50} />
+              </div>
+              <div>
+                <h2 id='bar-names'>CSS</h2>
+                <ProgressBar animated now={50} />
+              </div>
+              <div>
+                <h2 id='bar-names'>Redux</h2>
+                <ProgressBar animated now={50} />
+              </div>
             </div>
           </div>
+
       </div>
-
     )
   }
 }
