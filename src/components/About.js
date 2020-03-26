67401ca5be12c073bc1468a39502ddf64b0792d3
Victor Choe <vchoe89@gmail.com>
2020-03-26 17:01:02 -0400

    removed tech section

--- src/components/About.js
+++ src/components/About.js
@@ -10,14 +10,16 @@ class About extends React.Component{
       <div id='about-page'>
         <div id='about__container'>
             <div>
-              <h1 id='headlines'>-About-</h1>
+              <h1 id='headlines'>About</h1>
               <h2 id='welcome-header'>Welcome! My Name is Victor Choe.</h2>
               <h2 id='welcome-header'>I am a Full Stack Developer.</h2>
             </div>
 
             <div id='icon-section'>
               <div id='fast'>
-                <i class="material-icons">speed</i>
+                <div>
+                  <i class="material-icons">speed</i>
+                </div>
                 <h3 id='secondary-header-fast'>fast</h3>
                 <p id='icon-desc'>Fast load times and high speed interaction.</p>
               </div>
@@ -33,49 +35,48 @@ class About extends React.Component{
                   <h3 id='secondary-header-dyn'>dynamic</h3>
                     <p id='icon-desc'>Interactive websites that come to life.</p>
               </div>
-
-              <div id='bar-section'>
-                <h2 id='tech-abilities'>Technical Abilities</h2>
-                  <div id='tech-bars'>
-                    <h2 id='icon-bars'>Ruby/Rails</h2>
-                    <ProgressBar animated now={50} />
-                  <div>
-                    <h2 id='bar-names'>JavaScript</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                  <div>
-                    <h2 id='bar-names'>React</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                  <div>
-                    <h2 id='bar-names'>HTML</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                  <div>
-                    <h2 id='bar-names'>CSS</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                  <div>
-                    <h2 id='bar-names'>Redux</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                  <div>
-                    <h2 id='bar-names'>React-Hooks</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                  <div>
-                    <h2 id='bar-names'>RESTful API</h2>
-                    <ProgressBar animated now={50} />
-                  </div>
-                </div>
-              </div>
             </div>
-
-
         </div>
       </div>
     )
   }
 }
+
+
+// <div id='bar-section'>
+//   <h2 id='tech-abilities'>Technical Abilities</h2>
+//     <div id='tech-bars'>
+//       <h2 id='icon-bars'>Ruby/Rails</h2>
+//       <ProgressBar animated now={50} />
+//     <div>
+//       <h2 id='bar-names'>JavaScript</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//     <div>
+//       <h2 id='bar-names'>React</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//     <div>
+//       <h2 id='bar-names'>HTML</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//     <div>
+//       <h2 id='bar-names'>CSS</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//     <div>
+//       <h2 id='bar-names'>Redux</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//     <div>
+//       <h2 id='bar-names'>React-Hooks</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//     <div>
+//       <h2 id='bar-names'>RESTful API</h2>
+//       <ProgressBar animated now={50} />
+//     </div>
+//   </div>
+// </div>
 
 export default About;
