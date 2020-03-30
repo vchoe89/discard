8b8f4a346d1f511b36e71be3b5f41f3a68d454d5
Victor Choe <vchoe89@gmail.com>
2020-03-30 16:46:06 -0400

    adjusted navbar

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Academia from '../images/academia.png'
+import { Card } from 'react-bootstrap';
 
 
 const ProjectCard = () => {
 
   return(
-    <h1>new card</h1>
+    <div id='projects'>
+      <h1 id='headlines'>Projects</h1>
+      <div id='multipage-container'>
+        <div id='project-cards'>
+          <Card>
+            <Card.Img variant="top" src={Academia} />
+            <Card.Body>
+              <h3 className='card-title'>Academia</h3>
+              <Card.Text>Some text here</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,27 +4,11 @@ import { Card } from 'react-bootstrap';
 import ProjectCard from './ProjectCard.js'
 import Academia from '../images/academia.png'
 
+const Projects = () => {
 
-class Projects extends React.Component{
-  render(){
-    return(
-      <div id='projects'>
-        <h1 id='headlines'>Projects</h1>
-        <div id='multipage-container'>
-          <div id='project-cards'>
-            <Card border='dark' style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={Academia} />
-              <Card.Body>
-                <h3 className='card-title'>Academia</h3>
-                <Card.Text>Some text here</Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-        </div>
-      </div>
-
-    )
-  }
+  return(
+    <ProjectCard />
+  )
 }
 
 export default Projects;
