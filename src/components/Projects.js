50a9f68c2276862e894c9c622cd3193fd8d75ef1
Victor Choe <vchoe89@gmail.com>
2020-03-30 15:48:18 -0400

    started project page adjustments

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,15 @@ import Academia from '../images/academia.png'
 class Projects extends React.Component{
   render(){
     return(
-      <div>
+      <div id='projects'>
         <h1 id='headlines'>Projects</h1>
-        <a href='https://github.com/vchoe89/academia'>
-          <Card>
-            <Card.Img variant="top" src={Academia} />
-          </Card>
-        </a>
-
+        <div id='multipage-container'>
+          <a href='https://github.com/vchoe89/academia'>
+            <Card>
+              <Card.Img variant="top" src={Academia} />
+            </Card>
+          </a>
+        </div>
       </div>
 
     )
