8a5f0f255dc9238745e70a91b51e86c0d3b0059d
Victor Choe <vchoe89@gmail.com>
2020-04-06 18:08:03 -0400

    adjusted button

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -16,7 +16,7 @@ const ProjectCard = () => {
               <h2 className='card-title'>Academia</h2>
               <Card.Text>A simple to use web application that connects parents and tutors/coaches in their local areas</Card.Text>
             </Card.Body>
-            <Button id='button'>Git</Button>
+              <a id='button' href='https://www.google.com' target='_blank'className='btn btn-primary'>Git</a>
           </Card>
         </div>
       </div>
