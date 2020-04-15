3996da46001810860467f6042f8fb57f9070c372
Victor Choe <vchoe89@gmail.com>
2020-04-14 20:29:51 -0400

    workin on welcome page finishes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import {Animated} from 'react-animated-css';
+import jswallpaper from '../images/jswallpaper.jpg'
 
 
 class Welcome extends React.Component {
@@ -11,11 +13,12 @@ class Welcome extends React.Component {
     return(
       <React.Fragment>
       <div className='welcome-page'>
-        <div id='welcome-container'>
-          <h1 id='welcome-title' >Welcome</h1>
-          <NavLink id='welcome-button' to='/about'>Click Here To Learn More</NavLink>
+          <img id='welcome-image' src='https://southafricatoday.net/wp-content/uploads/2018/10/1_-xlCG5r86ocNFhC5iE658A.jpeg' />
+          <div id='welcome-container'>
+              <h1 id='welcome-title' >Welcome</h1>
+              <NavLink id='welcome-button' to='/about'>Click Here To Learn More</NavLink>
+          </div>
         </div>
-      </div>
       </React.Fragment>
     )
   }
